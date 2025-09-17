e3cc27cf9a5ad4773c3618089424ba4159fa2fbf
co-den <iamagugbueikenna@gmail.com>
2025-09-17 21:58:41 +0100

    userController added

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -10,13 +10,5 @@ router.post("/register", authController.register);
 // LOGIN
 router.post("/login", authController.login);
 
-//USER PROFILE
-router.get("/profile", authController.getProfile);
-
-// UPDATE PROFILE
-router.put("/profile", authController.updateProfile);
-
-// DELETE PROFILE
-router.delete("/profile", authController.deleteProfile);
 
 module.exports = router;
